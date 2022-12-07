dbbce719db261630817200333efe42e5628e5896
Marcus Ayers <marcayers27@gmail.com>
2022-12-07 17:05:49 -0500

    new job and render

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,10 @@
 class JobsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def create 
+    new_job = Job.create(employer_name: "company x", employer_description: "we do xyx", job_title: "you do this", 
+      job_description: "do it good", year_of_experience: 10, base_salary: 50000) 
+      
+
+    render json: {job: new_job}
+  end
 end
