d693cca826d44a8cde815b3c18dab889835fcfdf
Marcus Ayers <marcayers27@gmail.com>
2022-12-07 17:52:11 -0500

    added routes and controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,36 @@
 class JobsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def create 
-    new_job = Job.create(employer_name: "company x", employer_description: "we do xyx", job_title: "you do this", 
-      job_description: "do it good", year_of_experience: 10, base_salary: 50000) 
+  # def create 
+  #   new_job = Job.create(employer_name: "company x", employer_description: "we do xyx", job_title: "you do this", 
+  #     job_description: "do it good", year_of_experience: 10, base_salary: 50000) 
       
 
-    render json: {job: new_job}
+  #   render json: {job: new_job}
+
+  # end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      #render 'job/create' 
+    end
   end
+
+  def index
+    @jobs = Job.all
+    render json: {job: Job.all}
+  end
+
+  def find
+    @jobs = Job.all
+    render json: {job: Job.find(2)}
+    # @job = Job.find_by(id: params[:id])
+    # render json: {job: Job.find(@job)}
+  end
+
+
+  def job_params
+    params.require(:task).permit(:content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  #get 'jobs', => 'jobs#index'
   post '/jobs', to: 'jobs#create'
+  get '/jobs', to: 'jobs#index'
+  get '/jobs/:id', to: 'jobs#find'
 end
